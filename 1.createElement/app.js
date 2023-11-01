29a658d41768aa1ca79acb93785e7adbe01fcb71
Evonne Hsu <evonne.hsu@kenkone.com>
2023-11-01 14:11:30 +0800

    practice to create own react

---
+++ 1.createElement/app.js
@@ -0,0 +1,67 @@
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) =>
+        typeof child === "object" ? child : createTextElement(child)
+      ),
+    },
+  };
+}
+
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+function render(element, container) {
+  console.log(element);
+  const dom =
+    element.type === "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(element.type);
+
+  const isProperty = (key) => key !== "children";
+
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach((name) => {
+      dom[name] = element.props[name];
+    });
+  // recursive
+  element.props.children.forEach((ele) => {
+    render(ele, dom);
+  });
+
+  container.appendChild(dom);
+}
+
+const Didact = {
+  createElement,
+  render,
+};
+
+// Didact.createElement(
+//   "div",
+//   { id: "foo" },
+//   Didact.createElement("a", null, "bar"),
+//   Didact.createElement("b")
+// );
+
+/** @jsx Didact.createElement */
+const element = (
+  <div id="foo">
+    <h2>asdfs</h2>
+    <a>bar</a>
+    <b />
+  </div>
+);
+
+const container = document.getElementById("root");
+Didact.render(element, container);
